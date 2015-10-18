63dd6e95d6d38ab835fc33b23866b413fcee3f65
sachinnitw1317 <sachinnitw1317@gmail.com>
2015-10-18 23:22:46 +0530

    post buttuons done(without likes)

---
+++ js/user_post.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+ 
+ function change(img) {
+    if (img.src.match("2.png")) {
+    	img.src=img.src.replace("2.png","1.png");
+    }
+    else{
+    img.src=img.src.replace("1.png","2.png");	
+    }
+ 	
+ }
+});
