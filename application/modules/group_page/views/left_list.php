498f2104db0fd37d8e4e9af2ea3cb0b22c557e7e
sachinnitw1317 <sachinnitw1317@gmail.com>
2015-10-30 00:03:31 +0530

    group page

---
+++ application/modules/group_page/views/left_list.php
@@ -0,0 +1,22 @@
+<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7" style="padding-left:15px;padding-right:20px;">
+	<div class="row">
+	<h3 style="margin-left:20px;">Your<mark style="color:#F0AD4E;"> Groups </mark></h3>	
+	<hr>
+	</div>
+	<div class="row">
+		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+			
+		</div>
+		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+			<img src="http://localhost/artphilic/images/login.png" class="img-responsive" style="border:1px solid lightgrey" alt="Image">
+			<h4 style="text-align:center"><mark style="color:grey">Artphillic</mark></h4>
+		</div>
+		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+			
+		</div>
+		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+			<img src="http://localhost/artphilic/images/login.png" class="img-responsive" style="border:1px solid lightgrey" alt="Image">
+			<h4 style="text-align:center"><mark style="color:grey">Artphillic</mark></h4>
+		</div>
+	</div>
+</div>
---
+++ application/modules/group_page/views/right_list.php
@@ -0,0 +1,32 @@
+<div class="col-xs-15 col-sm-15 col-md-15 col-lg-15" style="padding-left:15px;padding-right:20px;border-left:1px solid lightgrey">
+	<div class="row">
+		<form action="" method="POST" class="form-horizontal" role="form">
+				<div class="form-group">
+					<legend>Add new Group <span class="glyphicon glyphicon-plus" style="color:#00BFF3;margin-left:10px;" aria-hidden="true"></span> </legend>
+				</div>
+				<div class="form-group">
+				 	<label for="input-id" class="col-sm-4">Group name</label>
+					<div class="col-sm-6 col-sm-offset-1">
+						<input type="text" name="" id="input" class="form-control" value="" required="required" pattern="" title="">
+					</div>
+				</div>
+				<div class="form-group">
+				 	<label for="input-id" class="col-sm-4">Add Friends</label>
+					<div class="col-sm-6 col-sm-offset-1">
+						<input type="text" name="" id="input" class="form-control" value="" required="required" pattern="" title="">
+					</div>
+				</div>
+				<div class="form-group">
+				 	<label for="input-id" class="col-sm-4">Upload profile pic</label>
+					<div class="col-sm-6 col-sm-offset-1">
+						<input type="file" class="filestyle" data-input="false">
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-sm-10 col-sm-offset-2">
+						<button type="submit" class="btn btn-primary">Submit</button>
+					</div>
+				</div>
+		</form>
+	</div>
+</div>
